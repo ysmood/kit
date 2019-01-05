c1273f9cfe8fc52e04a61c07c82975415bfbed4c
Yad Smood <y.s.inside@gmail.com>
2019-01-06 07:58:01 +0900

    add: retry

--- utils.go
+++ utils.go
@@ -7,6 +7,7 @@ import (
 	"go/build"
 	"os"
 	"sync"
+	"time"
 
 	"github.com/mgutz/ansi"
 )
@@ -85,4 +86,31 @@ func Try(fn func()) (err interface{}) {
 	fn()
 
 	return err
+}
+
+// Retry retry function after a duration for several times
+func Retry(times int, wait time.Duration, fn func()) (errs []interface{}) {
+	var try func(int)
+
+	try = func(countdown int) {
+		defer func() {
+			if r := recover(); r != nil {
+				errs = append(errs, r)
+				if countdown <= 1 {
+					return
+				}
+				time.Sleep(wait)
+				try(countdown - 1)
+			}
+		}()
+
+		fn()
+	}
+
+	try(times)
+
+	if len(errs) < times {
+		return nil
+	}
+	return errs
 }
--- utils_test.go
+++ utils_test.go
@@ -3,6 +3,7 @@ package gokit_test
 import (
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 
@@ -25,4 +26,39 @@ func TestE(t *testing.T) {
 	}()
 
 	g.E(g.Exec([]string{"exitexit"}, nil))
+}
+
+func TestRetry(t *testing.T) {
+	count := 0
+	errs := g.Retry(3, 30*time.Millisecond, func() {
+		count = count + 1
+	})
+
+	assert.Equal(t, true, errs == nil)
+	assert.Equal(t, 1, count)
+}
+
+func TestRetryHalf(t *testing.T) {
+	count := 0
+	errs := g.Retry(5, 30*time.Millisecond, func() {
+		count = count + 1
+
+		if count < 3 {
+			panic(count)
+		}
+	})
+
+	assert.Equal(t, true, errs == nil)
+	assert.Equal(t, 3, count)
+}
+
+func TestRetry3Times(t *testing.T) {
+	count := 0
+	errs := g.Retry(3, 30*time.Millisecond, func() {
+		count = count + 1
+		panic(count)
+	})
+
+	assert.Equal(t, []interface{}{1, 2, 3}, errs)
+	assert.Equal(t, 3, count)
 }
